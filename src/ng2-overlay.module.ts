c2ad7d9ed080b5c79fab04e6070da1ef09acbdd2
Allen Kim <allenhwkim@gmail.com>
2017-02-11 20:37:47 -0500

    0.7.1 export Ng2OverlayManager from ng2-overlay-module.ts

--- src/ng2-overlay.module.ts
+++ src/ng2-overlay.module.ts
@@ -5,10 +5,14 @@ import { CommonModule  } from '@angular/common';
 import { Ng2OverlayManager } from './ng2-overlay-manager';
 import { Ng2OverlayDirective } from './ng2-overlay.directive';
 
+export {
+  Ng2OverlayManager
+};
+
 @NgModule({
   imports: [ CommonModule, FormsModule ],
   declarations: [Ng2OverlayDirective],
   providers: [ Ng2OverlayManager ],
-  exports: [ Ng2OverlayDirective ]
+  exports: [ Ng2OverlayDirective  ]
 })
 export class Ng2OverlayModule {};
